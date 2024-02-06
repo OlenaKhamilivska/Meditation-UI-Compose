7b95863413f83a86baa03f20d91f7307cbe34746
OlenaKhamilivska <stadni4yk.elena@gmail.com>
2024-02-06 14:54:14 +0000

    add commment

---
+++ app/src/main/java/com/example/meditationuicompose/BottomMenuResource.kt
@@ -0,0 +1,8 @@
+package com.example.meditationuicompose
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuResource(
+    val title: String,
+    @DrawableRes val iconId: Int
+)
---
+++ app/src/main/java/com/example/meditationuicompose/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.meditationuicompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.meditationuicompose.ui.theme.MeditationUIComposeTheme
+import com.example.meditationuicompose.ui.theme.MeditationUIYouTubeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUIYouTubeTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
